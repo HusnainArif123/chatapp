a6ffafc297a843620bfab5f7cf0534f977157973
HusnainArif123 <husnainarif730@gmail.com>
2025-04-08 16:52:23 +0500

    fixed ui issue

--- src/app/components/SubscriptionCard.tsx
+++ src/app/components/SubscriptionCard.tsx
@@ -21,10 +21,12 @@ const CaseCard: React.FC<CaseCardProps> = ({
     <div className="relative w-80 h-48 p-6 rounded-[40px] bg-gradient-to-br from-[#ec4899] via-[#DFAACB] to-[#ec4899] shadow-md overflow-hidden">
       <Icon className="absolute w-72 h-72 text-white/10 -left-10 -top-16" />
 
-      <div className="relative z-10 flex flex-col justify-between h-full">
+      <div className="relative z-10 flex flex-col justify-between h-40">
         <div>
-          <h2 className="text-lg font-semibold text-gray-800 mb-1">{title}</h2>
-          <p className="text-sm text-gray-600">{description}</p>
+          <h2 className="text-lg font-semibold text-gray-800 mb-1 truncate">
+            {title}
+          </h2>
+          <p className="text-sm text-gray-600 line-clamp-4">{description}</p>
         </div>
 
         <div className="mt-4">
